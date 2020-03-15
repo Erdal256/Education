553f8f29c2ca4a3fbed4187d459ff25c3dafa676
Erdal256 <erdall.2693@gmail.com>
2020-03-15 22:52:25 +0300

    HesapMakinesi

---
+++ HesapMakinesi/Javascript/Javascript/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Javascript.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Javascript1()
+        {
+            return View();
+        }
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        public IActionResult Calculator2()
+        {
+            return View();
+        }
+    }
+}
