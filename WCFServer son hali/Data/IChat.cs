e849e8820e1426bf935fb0d5ed95a9fbe04e0984
Erdal256 <erdall.2693@gmail.com>
2020-04-09 23:35:01 +0300

    WCF

---
+++ WCFServer son hali/Data/IChat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.ComIntegration;
+
+namespace Data
+{
+    public class Singletons
+    {
+        public static List<MessageDto> Messages = new List<MessageDto>();
+    }
+    [ServiceContract]
+    public interface IChat
+    {
+        [OperationContract]
+
+        void SendMessage(string nickname, string message);
+
+        [OperationContract]
+
+        List<MessageDto> GetAllMessages();
+    }
+
+    public class ChatService : IChat
+    {
+
+
+        public void SendMessage(string nickname, string message)
+        {
+            Singletons.Messages.Add(new MessageDto()
+            {
+                Id = Guid.NewGuid(),
+                NickName = nickname,
+                Message = message,
+                SendDate = DateTime.Now
+            });
+            Console.WriteLine(nickname + ":" + message);
+            string path = @"C:\Users\erdal\Desktop\YMS8518\WCF-master\log.txt";
+            
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = nickname + ":" + message +"-->"+ DateTime.Now + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+            else
+            {
+                //This text is alwasy added, making the file longer over time 
+                // if it is not deleted.
+                string appendText = nickname + ":" + message + "-->" + DateTime.Now + Environment.NewLine;
+                File.AppendAllText(path, appendText);
+            }
+
+            //File.writeralltext
+            //file.appendtext
+
+
+
+        }
+        public List<MessageDto> GetAllMessages()
+        {
+            return Singletons.Messages;
+        }
+    }
+
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+
+        public DateTime SendDate { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
