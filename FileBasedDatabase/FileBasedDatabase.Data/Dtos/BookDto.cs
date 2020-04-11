ccee55c17153867a4efd2d9fedbaea276ea539ad
Erdal256 <erdall.2693@gmail.com>
2020-04-11 15:10:06 +0300

    ok

---
+++ FileBasedDatabase/FileBasedDatabase.Data/Dtos/BookDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FileBasedDatabase.Data.Dtos
+{
+    public class BookDto
+    {
+        public  Guid Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
